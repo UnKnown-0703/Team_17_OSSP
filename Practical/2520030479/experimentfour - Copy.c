017d228b351163b50a07df454ff7149456005978
premkumar479 <premkumarviswanadhapalli@gmail.com>
2026-08-11 09:12:16 +0530

    Add files via upload

---
+++ Practical/2520030479/experimentfour - Copy.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<unistd.h>
+int main()
+{
+pid_t pid;
+pid == fork();
+if(pid ==0)
+{
+printf("Child PID = %d\n", getpid());
+}
+else if (pid > 0)
+{
+printf("Parent PID = %d\n", getpid());
+printf("Child PID = %d\n", pid);
+}
+else
+{
+printf("Fork failed!\n");
+}
+return 0;
+}
+
+
+
