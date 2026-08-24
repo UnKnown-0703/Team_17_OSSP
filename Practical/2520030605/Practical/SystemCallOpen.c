ce301d5a694226499c8ecbdf0f36690e50f3c687
Tata Bharghava Sai Sunil <sunil.tatabharghava@gmail.com>
2026-08-24 10:58:22 +0530

    ..

---
+++ Practical/2520030605/Practical/SystemCallOpen.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+	int main(){
+
+	 int fd;
+	 fd = open("input.txt",O_RDONLY);
+	   if(fd == -1){
+	     perror("Error opening file");
+	      return 1;
+	   }
+
+	 printf("File opened successfully.\n");
+	 printf("File Discription: %d\n", fd);
+
+	 close(fd);
+
+	return 0;
+	}
